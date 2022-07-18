96e155fa0f884bd38c47b7123b2ef38f5f0b9b39
FPantring <90556171+FPantring@users.noreply.github.com>
2022-07-18 14:06:38 +0100

    Added some more comments

--- Pileup_Processing.py
+++ Pileup_Processing.py
@@ -4,10 +4,11 @@
 # This script extracts all alternative mapping locations from the annotations of the reads in the pileup of all mapping quality variant loci.
 # It needs to be called with the paths and names to three input files: the .bam file, the .fa reference file and 
 # the ground truth map file which lists all mapping quality variants with including information about variants spiked in at that location.
-# When running this script with real data instead of simulated, instead of the ground truth map a tap seperated file should be uploaded,
+# When running this script with real data instead of simulated, instead of the ground truth map a tap separated file should be uploaded,
 # which features the mapping quality variant positions in the first column in the format chr1:12345678 and the single base substitution at that position
-# in the second column in the format R>V, with R being the reference allele at this position and V the variant allele.  
-# Sor for example: python3 Pileup_Processing.py T_WGS.bam GRCh38.d1.vd1.fa WGS_gtMap.txt > output.txt
+# in the second column in the format R>V, with R being the reference allele at this position and V the variant allele. Note that this file should include a header, 
+# otherwise the first row of the file will be missed in the analysis.
+# Example call of this script: python3 Pileup_Processing.py T_WGS.bam GRCh38.d1.vd1.fa WGS_gtMap.txt > output.txt
 
 # This script creates a lot of output which should be saved in a log file (using the command shown above) and can be manually reviewed for troubleshooting purposes
 # However, truly relevant for the Analysis Pipeline are only the files Alt_locs.csv and excluded.txt which will be produced at the end of the script
@@ -36,11 +37,10 @@ from pyfaidx import Faidx
 fa = Faidx(sys.argv[2])
 
 
-# create empty dictornaries that will be filled in later loops
+# create empty dictionaries that will be filled in later loops
 poslist = []
 refdict = {}
 alleledict = {}
-#df = pd.DataFrame(columns=["init", "new", "cigar"])
 appended_data = {}
 
 
@@ -52,7 +52,7 @@ appended_data = {}
 # 	select False if the function is applied to adjust according to the CIGAR string of an alternative alignment location.
 def base_pos(l, startpos, cigar, orig):
     # access the position of the bases at the variant position of interest and the corresponding reference position
-    # to get this position it is necessary to substract the start position of the read alignment to the reference
+    # to get this position it is necessary to subtract the start position of the read alignment to the reference
     # from the position of the variant (the second entry of the list loc -1 (subtract one to correct to 0-based))
     # Get base position at query_alignment_sequence:
     distance = l-startpos
@@ -60,7 +60,7 @@ def base_pos(l, startpos, cigar, orig):
     if re.findall('I|D|S', cigar) != []: # check if insertion or deletion present
         start = 0
         dl = []
-        # seperate the string at the number before a D or I to extract all deletions and insertions:
+        # separate the string at the number before a D or I to extract all deletions and insertions:
         for num1, i_or_d in re.findall('(\d+)([IDSM])', cigar):
             if i_or_d not in 'IDS':
                 # add the number of matches and substitutions before the indel 
@@ -71,10 +71,10 @@ def base_pos(l, startpos, cigar, orig):
             if num1:
                 start += int(num1)
         for j in dl:
-            # if the indel occured after the loci in question, no adjustment of the position is necessary)
+            # if the indel occurred after the loci in question, no adjustment of the position is necessary)
             if distance < j[2]:
                 continue
-            # if a region in the beginning is sofclipped, this is likely a result of the vairant in the read,
+            # if a region in the beginning is sofclipped, this is likely a result of the variant in the read,
             # because due to the mismatch in the beginning the alignment software assumes softclipping.
             # Since the "coordinate shown in SAM is the position of the first aligned base" (Li, Heng et al. 
             # “The Sequence Alignment/Map format and SAMtools.” Bioinformatics (Oxford, England) 
@@ -96,21 +96,21 @@ def base_pos(l, startpos, cigar, orig):
                 print("\n", "DELETION!!!!!!!", cigar, distance, "\n")
                 return "DELETION"
                     
-            # if the indel occured before the loci in question (or at the loci in the case of an insertion), 
+            # if the indel occurred before the loci in question (or at the loci in the case of an insertion), 
             # adjust the position number:
             else:
-                # in case of an insertion, substract the number of inserted bases from the current position 
+                # in case of an insertion, subtract the number of inserted bases from the current position 
                 # when adjusting for an alternative location. 
                 if j[1] == "I":
                     if orig == False:
                         distance = distance - int(j[0])
                     else: # When adjusting for the original CIGAR string, add the number of inserted bases
                         distance = distance + int(j[0])
-                # in case of a deletion, add the number of deleted bases to the current position when adjuasting for an alternative location
+                # in case of a deletion, add the number of deleted bases to the current position when adjusting for an alternative location
                 elif j[1] == "D":
                     if orig == False:
                         distance = distance + int(j[0])
-                    else: # When adjusting for the original CIGAR string, substract the number of deleted bases
+                    else: # When adjusting for the original CIGAR string, subtract the number of deleted bases
                         distance = distance - int(j[0])
     if cigar != "76M":
        # if cigar not in appended_data.keys():
@@ -127,20 +127,20 @@ with open(sys.argv[3]) as gtMap:
         # make the string location accessible:
         loc = VafLoc.split(":")
 
-        # access the read pileup at a specific postion:
+        # access the read pileup at a specific position:
         # REMEMBER: take (position-1, position) instead of (position, position+1), because coordinates are 0-based
-        # access the alternative hits with .get_tag("XA") and concatenate all alterative hits to one list
+        # access the alternative hits with .get_tag("XA") and concatenate all alternative hits to one list
         # each list element then includes: chromosome and position of the read alignment start position at the alternative location, 
         # CIGAR string of the alignment at the alternative location, edit distance (number of nucleotide changes), 
         # the position of the variant within the read, the reverse position within the read (position when counted from the end of the read),
-        # the inital location of the read in the format chr:positionnumber, the read sequence and the CIGAR string of the original alignment
+        # the initial location of the read in the format chr:positionnumber, the read sequence and the CIGAR string of the original alignment
         element=[re.sub("[+-]", "", s) + "," + str(base_pos(l = (int(loc[1]) - 1), startpos = pileupread.alignment.reference_start,
                                                             cigar = pileupread.alignment.cigarstring, orig = True)) + "," +
                  str(len(pileupread.alignment.query_sequence)-base_pos(l = (int(loc[1]) - 1), startpos = pileupread.alignment.reference_start,
                                                                        cigar = pileupread.alignment.cigarstring, orig = True) - 1) +
                  "," + loc[0] + ":" + loc[1] + "," + str(pileupread.alignment.query_sequence) + "," + str(pileupread.alignment.cigarstring)
                  for pileupcolumn in samfile.pileup(loc[0], (int(loc[1]) - 1), int(loc[1])) for pileupread in pileupcolumn.pileups
-                 # check whether the position in question has no alternative aligments in the genome
+                 # check whether the position in question has no alternative alignments in the genome
                  # and whether the position is deleted. If so, this read is not relevant:
                  if pileupread.alignment.has_tag("XA") and base_pos(l = (int(loc[1]) - 1), startpos = pileupread.alignment.reference_start,
                                                                     cigar = pileupread.alignment.cigarstring, orig = True) != "DELETION" and \
@@ -150,7 +150,7 @@ with open(sys.argv[3]) as gtMap:
         poslist = poslist + element
 
                      
-# Convert the list into a dataframe where the information is seperated into columns
+# Convert the list into a data frame where the information is separated into columns
 # dis is short for edit distance, org_cig stands for the CIGAR string of the original alignment
 posframe = pd.DataFrame(columns = ["chromosome", "position", "CIGAR", "dis", "pos", "reverse", "initial pos", "seq", "org_cig"], 
                         data=[col.split(",") for col in poslist[0:]])
@@ -166,7 +166,7 @@ posframe = posframe.drop(posframe[(posframe["pos"] == "DELETION") | (posframe["r
 
 # check for all alternative positions whether the reference corresponds to a reads forward, reverse, complement or reverse complement strand:
 def revcheck(i):
-    # it is important to adjust the position according to the CIGAR string, because if some bases are anotated as soft clipped
+    # it is important to adjust the position according to the CIGAR string, because if some bases are annotated as soft clipped
     # the alignment of the read will be shifted by the amount of softclipped bases in the beginning
     if (int(i["position"]) + base_pos(0, 0, i["CIGAR"], orig = False)) > 0 and (int(i["position"]) + base_pos(0, 0, i["CIGAR"], orig = False)) > 0:
         choices = {str(fa.fetch(i["chromosome"], (int(i["position"]) + base_pos(0, 0, i["CIGAR"], orig = False)), 
@@ -192,7 +192,7 @@ def poscalc(i):
     
 print(posframe.apply(revcheck, axis = 1))
 
-# create an additional column in the dataframe, which includes the actual variant position:
+# create an additional column in the data frame, which includes the actual variant position:
 posframe['actual pos'] = posframe.apply(poscalc, axis = 1)
 
 # drop duplicated rows to minimise computational time:
@@ -225,7 +225,7 @@ def varbase(i):
 
 
             
-# apply the function to the dataframe of unique rows       
+# apply the function to the data frame of unique rows       
 posframe.apply(varbase, axis = 1)  
 
 # create a list including all loci for which no alternate loci with the variant allele exist
